41be526613842d0481ccc43b2cdd7f987089005b
KHY90 <power4206@gmail.com>
2024-09-05 17:45:39 +0900

    feat:naverlogin,curationAPI

---
+++ Omniscient-Back/src/main/java/com/omniscient/omniscientback/login/naver/controller/NaverController.java
@@ -0,0 +1,12 @@
+package com.omniscient.omniscientback.login.naver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class NaverController {
+
+//    @GetMapping("/login/naver")
+//    public CustomResponseEntity<UserResponse.Login> loginByNaver(@RequestParam(name = "code") String code) {
+//        return CustomResponseEntity.success(userService.loginByOAuth(code, NAVER));
+//    }
+}
