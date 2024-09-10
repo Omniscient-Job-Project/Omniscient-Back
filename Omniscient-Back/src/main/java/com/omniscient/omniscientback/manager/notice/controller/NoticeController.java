9a84fe557551eb9d4c4e8b0703a39fb8401b614d
Jin-tonix <jinheemok815@gmail.com>
2024-09-10 17:32:19 +0900

    fix noticeController

--- Omniscient-Back/src/main/java/com/omniscient/omniscientback/manager/notice/controller/NoticeController.java
+++ Omniscient-Back/src/main/java/com/omniscient/omniscientback/manager/notice/controller/NoticeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
+// 확인
 @RestController
 @RequestMapping("/api/v1/notice")
 @CrossOrigin(origins = "http://localhost:8083")
